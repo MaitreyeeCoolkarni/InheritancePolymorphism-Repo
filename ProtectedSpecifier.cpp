2c5c6e3c0291f89d6d047282d248e61489a2a253
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-09-04 12:26:37 +0530

    Create ProtectedSpecifier.cpp

---
+++ ProtectedSpecifier.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+using namespace std;
+#include<string>
+
+//Base class
+class Animal {
+    
+private:
+   int lifespan=2;
+   
+protected:
+ int Age=10;
+    
+public:
+    string Name;
+    int legs;
+
+    Animal(string str1, int x) {
+        Name = str1;
+        legs = x;
+    }
+
+    void sound() {
+        cout << "Animal sound";
+    }
+};
+
+class Dog : public Animal {
+public:
+    Dog(string str1, int x) : Animal(str1, x) {}
+    
+    void sound()
+    {
+        cout<<"Woof"<<endl;
+    }
+    
+    
+    void age()
+    {
+        cout<<Age<<endl;
+        
+    }
+};
+
+int main() {
+    Dog D1("J", 4);
+    D1.sound();
+    D1.age();
+   
+    
+    
+    
+}
