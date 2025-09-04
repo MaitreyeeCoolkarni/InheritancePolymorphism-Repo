7979ca523468a2425c9b9687467b613b7804fca1
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-09-04 11:41:34 +0530

    Create Multipleinheritance.cpp

---
+++ Multipleinheritance.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#include<string>
+
+//Base class
+class Animal {
+public:
+    string Name;
+    int legs;
+
+    Animal(string str1, int x) {
+        Name = str1;
+        legs = x;
+    }
+
+    void sound() {
+        cout << "Animal sound";
+    }
+};
+
+class Mammal {
+public:
+
+   int middlebones=3;
+ 
+};
+
+
+
+
+
+class Dog : public Animal,public Mammal{
+public:
+    Dog(string str1, int x) : Animal(str1, x) {}
+    
+    void sound()
+    {
+        cout<<"Woof"<<endl;
+    }
+  
+    
+};
+
+int main() {
+    Dog D1("J", 4);
+    D1.sound();
+    cout<<D1.middlebones<<endl;
+    
+}
